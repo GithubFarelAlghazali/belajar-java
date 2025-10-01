35f527eaee8fa57ab392ccdf9f440bff32a08a06
GithubFarelAlghazali <farelgithub@gmail.com>
2025-10-01 20:18:30 +0700

    adding MODUL_3: KonversiSuhu

---
+++ MODUL_3/KonversiSuhu.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class KonversiSuhu{
+    public static void main(String[]args){
+    Scanner scanner = new Scanner(System.in);
+
+    System.out.print("Masukkan suhu dalam Celcius: ");
+    double celcius= scanner.nextDouble();
+
+    double fahrenheit = (celcius * 9/5) + 32;
+    double reamur = celcius * 4/5;
+    double kelvin = celcius + 273.15;
+
+    System.out.println("Suhu dalam Fahrenheit: "+ fahrenheit);
+    System.out.println("Suhu dalam Reamur: "+ reamur);
+    System.out.println("Suhu dalam Kelvin: "+ kelvin);
+    }
+}
